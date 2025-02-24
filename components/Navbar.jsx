97f4f8af98bec064b20f516a861b8a528aa274cc
Mario Gutierrez <gmmalberto11@gmail.com>
2025-02-23 21:54:08 -0400

    First commit

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -12,12 +12,9 @@ const Navbar = () => (
     className={`${styles.xPaddings} py-8 relative`}
   >
     <div className="absolute w-[50%] inset-0 gradient-01" />
-    <div className={`${styles.innerWidth} mx-auto flex justify-between gap-8`}>
-      <img src="/search.svg" alt="search" className="w-[24px] h-[24px] object-contain" />
-
+    <div className={`${styles.innerWidth} mx-auto flex justify-center gap-8`}>
       <img src="/logo.svg" alt="logo" className="w-[80px] object-contain" />
 
-      <img src="/menu.svg" alt="menu" className="w-[24px] h-[24px] object-contain" />
     </div>
   </motion.nav>
 );
